bff9ccb719a5f8e812bc8f777a30a42fd8697097
Eduard Braun <Eduard.Braun2@gmx.de>
2015-04-02 02:21:49 +0200

    Add an icon.

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 rm greasyscripts.xpi
-7z a -mx -mtc- -r -i!install.rdf -i!chrome.manifest -i!bootstrap.js -i!options.xul -i!content/ -i!locale/ -i!skin/ greasyscripts.xpi
+7z a -mx -mtc- -r  -i!content/ -i!locale/ -i!skin/ -i!install.rdf -i!chrome.manifest -i!bootstrap.js -i!options.xul  -i!icon.png -i!icon64.png greasyscripts.xpi
